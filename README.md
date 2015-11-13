f28f5fb9b84c063dcb610b497b695f63d6f7cbba
Harry Maclean <harryjmaclean@googlemail.com>
2015-11-13 09:13:04 +0000

    simple TCP server

---
+++ README.md
@@ -0,0 +1,12 @@
+To use:
+
+```
+cargo run
+```
+
+In a separate shell:
+```
+nc 127.0.0.1 33333
+```
+
+This connects to the rust server that you just started.
---
+++ src/main.rs
@@ -0,0 +1,28 @@
+use std::net::{TcpListener, TcpStream};
+use std::io::Read;
+use std::thread;
+
+fn main() {
+    let listener = TcpListener::bind("127.0.0.1:33333").unwrap();
+
+    for stream in listener.incoming() {
+        match stream {
+            Ok(stream) => {
+                thread::spawn(move|| {
+                    handle_request(stream)
+                });
+            }
+            Err(e) => { println!("connection failed"); }
+        }
+    }
+
+    drop(listener);
+}
+
+fn handle_request(stream: TcpStream) {
+    println!("{:?}", stream);
+    //let mut buffer = String::new();
+    //try!(stream.read_to_string(&mut buffer));
+    //println!("{:?}", buffer);
+
+}
