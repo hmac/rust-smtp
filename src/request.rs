353c1180c67a9ad9f09c346f5b546d2649679a2d
Harry Maclean <harrymaclean@gocardless.com>
2015-11-24 14:00:01 +0000

    wip

--- src/request.rs
+++ src/request.rs
@@ -8,81 +8,76 @@ pub fn handle_request(mut stream: TcpStream) {
     let mut email = Email {to: None, from: None, body: None};
     loop {
         buf.read_line(&mut request_string);
-        let maybe_command = parse_request(&request_string);
-        if maybe_command.is_some() {
-            match maybe_command.unwrap() {
-                Command::Helo(addr) => {
-                    println!("Got HELO from {:?}, sending 250", addr);
-                    buf.write("250 Hello ".as_bytes());
-                    buf.write(addr.as_bytes());
-                    buf.write("\n".as_bytes());
-                },
-                Command::Mail(from) => {
-                    println!("Got MAIL, sending 250");
-                    println!("From: {:?}", from);
-                    buf.write("250 Ok \n".as_bytes());
-                    email.from = Some(from);
-                },
-                Command::Rcpt(recipient) => {
-                    println!("Got RCPT, sending 250");
-                    println!("Recipient: {:?}", recipient);
-                    buf.write("250 Ok \n".as_bytes());
-                    email.to = Some(recipient);
-                },
-                Command::Data => {
-                    let mut body: Vec<String> = Vec::new();
-                    let mut body_string = String::new();
-                    buf.write("354 End data with <CR><LF>.<CR><LF>\n".as_bytes());
-                    buf.flush();
-                    'data: loop {
-                        buf.read_line(&mut body_string);
-                        println!("{:?}", body_string);
-                        if body_string == ".\n" {
-                            println!("Detected end of mail body");
-                            break 'data;
-                        }
-                        body.push(body_string.clone());
-                        buf.flush();
-                        body_string.clear();
+        match parse_request(&request_string) {
+            Ok(Command::Helo(addr)) => {
+                println!("Got HELO from {:?}, sending 250", addr);
+                buf.write("250 Hello ".as_bytes());
+                buf.write(addr.as_bytes());
+                buf.write("\n".as_bytes());
+            },
+            Ok(Command::Mail(from)) => {
+                println!("Got MAIL, sending 250");
+                println!("From: {:?}", from);
+                buf.write("250 Ok \n".as_bytes());
+                email.from = Some(from);
+            },
+            Ok(Command::Rcpt(recipient)) => {
+                println!("Got RCPT, sending 250");
+                println!("Recipient: {:?}", recipient);
+                buf.write("250 Ok \n".as_bytes());
+                email.to = Some(recipient);
+            },
+            Ok(Command::Data) => {
+                let mut body: Vec<String> = Vec::new();
+                let mut body_string = String::new();
+                buf.write(ResponseCode::StartMailInput.to_string().as_bytes());
+                buf.flush();
+                'data: loop {
+                    buf.read_line(&mut body_string);
+                    println!("{:?}", body_string);
+                    if body_string == ".\n" {
+                        println!("Detected end of mail body");
+                        break 'data;
                     }
-                    email.body = Some(body.concat());
+                    body.push(body_string.clone());
+                    buf.flush();
+                    body_string.clear();
                 }
-            };
+                email.body = Some(body.concat());
+            },
+            Err(response_code) => {
+                buf.write(&response_code.to_string().as_bytes());
+                println!("Command not recognised: {:?}", request_string);
+            }
         }
-        else {
-            println!("Command not recognised: {:?}", request_string);
-        };
         buf.flush();
         request_string.clear();
     };
 }
 
-fn validate_request(command: Command) -> Result<(), ResponseCode> {
-}
-
-fn parse_request(req: &str) -> Option<Command> {
+fn parse_request(req: &str) -> Result<Command, ResponseCode> {
     // The command should be a 4 character string like HELO or MAIL
     // It is followed by a space, then any data
-    if req.len() < 4 { return None };
+    if req.len() < 4 { return Err(ResponseCode::CommandUnrecognised) };
     let command_str = &req[0..4];
     match command_str {
-        "HELO" => Some(Command::Helo(req[5..].to_string())),
+        "HELO" => Ok(Command::Helo(req[5..].to_string())),
         "MAIL" => {
             let addr = parse_from_address(&req[5..]);
             match addr {
-                Some(address) => Some(Command::Mail(address)),
-                None          => None
+                Some(address) => Ok(Command::Mail(address)),
+                None          => Err(ResponseCode::ArgumentError)
             }
         },
         "RCPT" => {
             let addr = parse_to_address(&req[5..]);
             match addr {
-                Some(address) => Some(Command::Rcpt(address)),
-                None          => None
+                Some(address) => Ok(Command::Rcpt(address)),
+                None          => Err(ResponseCode::ArgumentError)
             }
         },
-        "DATA" => Some(Command::Data),
-        _      => None
+        "DATA" => Ok(Command::Data),
+        _      => Err(ResponseCode::CommandUnrecognised)
     }
 }
 
@@ -125,9 +120,21 @@ struct Email {
     body: Option<String>
 }
 
-// Not used yet
+#[derive(Debug)]
 enum ResponseCode {
     Ok,
     StartMailInput,
-    CommandUnrecognised
+    CommandUnrecognised,
+    ArgumentError
+}
+
+impl ToString for ResponseCode {
+    fn to_string(&self) -> String {
+        match self {
+            &ResponseCode::Ok => "250 Requested mail action completed.".to_string(),
+            &ResponseCode::StartMailInput => "354 End data with <CR><LF>.<CR><LF>".to_string(),
+            &ResponseCode::CommandUnrecognised => "500 Syntax error, command unrecognised.".to_string(),
+            &ResponseCode::ArgumentError => "501 Syntax error in command arguments".to_string()
+        }
+    }
 }
